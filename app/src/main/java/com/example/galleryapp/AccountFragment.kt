ee1d72cc2662083140cfc1e915b88ed1c277e921
Karina <kmirova731@gmail.com>
2023-11-22 16:03:00 +0600

    navigation to login

--- app/src/main/java/com/example/galleryapp/AccountFragment.kt
+++ app/src/main/java/com/example/galleryapp/AccountFragment.kt
@@ -1,17 +1,23 @@
 package com.example.galleryapp
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintSet.Motion
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.example.galleryapp.databinding.FragmentAccountBinding
+import com.example.galleryapp.utils.setSafeOnClickListener
 
 
 class AccountFragment : Fragment() {
     private lateinit var binding: FragmentAccountBinding
+
+    private var isShow = true
+    private var scrollRange = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +31,19 @@ class AccountFragment : Fragment() {
 //        binding.toolBar.inflateMenu(R.menu.account_app_bar_logged_user)
 
 
+        binding.toolbar.trailingIconAction = {
+//            Toast.makeText(requireContext(), "${binding.toolbar.scrollY}", Toast.LENGTH_SHORT).show()
+            Navigation.findNavController(binding.root).navigate(R.id.loginFragment)
+        }
+
+//        binding.toolbar.setSafeOnClickListener {
+//            if
+//        }
+
         return binding.root
+    }
+
+    private fun blockToolbar(motion: MotionEvent): Boolean {
+        return false
     }
 }
